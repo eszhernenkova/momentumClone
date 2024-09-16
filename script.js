42c293be85f5f0da00c3d1c894aee1ee5cbc6bef
eszhernenkova <zhernenkova.ekaterina@gmail.com>
2024-09-16 22:37:47 +0400

    weather-done version

--- script.js
+++ script.js
@@ -1,19 +1,89 @@
 document.addEventListener('DOMContentLoaded', () => {
     const weatherBlock = document.getElementById('weather-block');
     const todoList = document.getElementById('todo-list');
-    const weatherIcon = document.getElementById('weather-icon');
     const todoToggle = document.getElementById('todo-toggle');
     const backgroundSlider = document.getElementById('background-slider');
+
+    const apiKey = "6784be0ca773a4f9d5d6f512f033dd17";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
     
-    weatherBlock.addEventListener('click', () => {
-        if (weatherBlock.classList.contains('collapsed')) {
-            weatherBlock.classList.remove('collapsed');
-            weatherBlock.classList.add('expanded');
+    const searchBox = document.querySelector(".search input"); 
+    const searchBtn = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon");
+    
+    const weatherStorageKey = 'weatherData';
+
+    const defaultCity = "Краснодар";
+
+    async function cherWeather(city) {
+        const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+        if(response.status == 404){
+            document.querySelector(".error").style.display="block";
+            document.querySelector(".weather").style.display="none";
         } else {
-            weatherBlock.classList.remove('expanded');
-            weatherBlock.classList.add('collapsed');
+            const data = await response.json();
+    
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp ) + "°c";
+    
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src = "images/clouds.png"
+            } else if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "images/clear.png"
+            } else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "images/rain.png"
+            } else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "images/drizzle.png"
+            } else if(data.weather[0].main == "Mist"){
+                weatherIcon.src = "images/mist.png"
+            }
+    
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display="none";
+        } 
+    }
+
+    cherWeather(defaultCity);
+
+    // function loadWeatherFromLocalStorage() {
+    //     const storedWeatherData = localStorage.getItem(weatherStorageKey);
+        
+    //     if (storedWeatherData) {
+    //         const data = JSON.parse(storedWeatherData);
+    //         displayWeather(data);
+    //     } else {
+    //         fetchWeather(defaultCity);
+    //     }
+    // }
+    
+    // // Отобразить погоду для города по умолчанию при загрузке страницы
+    // loadWeatherFromLocalStorage();
+    
+    weatherBlock.addEventListener('click', (event) => {
+        if (event.target !== searchBox && event.target !== searchBtn) {
+            if (weatherBlock.classList.contains('collapsed')) {
+                weatherBlock.classList.remove('collapsed');
+                weatherBlock.classList.add('expanded');
+                document.querySelector(".search").style.display = "flex"; // Показываем инпут
+            } else {
+                weatherBlock.classList.remove('expanded');
+                weatherBlock.classList.add('collapsed');
+                document.querySelector(".search").style.display = "none"; // Скрываем инпут
+            }
         }
     });
+    
+    searchBtn.addEventListener("click", ()=>{
+        console.log('Search button clicked');
+        cherWeather(searchBox.value);
+    });
+    
+    document.addEventListener('keyup', function(event){
+       if(event.code === 'Enter'){
+            cherWeather(searchBox.value);
+       }
+    })
 
     todoToggle.addEventListener('click', () => {
         if (todoList.classList.contains('collapsed')) {
